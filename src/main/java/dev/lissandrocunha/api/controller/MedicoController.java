1d71117ed07a7121811a7fc5bd674c40619d6da5
lissandro.cunha <lissandro.cunha@ifood.com.br>
2023-03-29 16:15:46 -0300

    - added migration for flyway; - added bean validation for DadosCadastroMedico.

--- src/main/java/dev/lissandrocunha/api/controller/MedicoController.java
+++ src/main/java/dev/lissandrocunha/api/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package dev.lissandrocunha.api.controller;
 
 import dev.lissandrocunha.api.medico.DadosCadastroMedico;
+import dev.lissandrocunha.api.medico.Medico;
+import dev.lissandrocunha.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/medicos")
+
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(
-            @RequestBody DadosCadastroMedico dados
+            @RequestBody @Valid DadosCadastroMedico dados
     ){
-        System.out.println(dados);
+        medicoRepository.save(new Medico(dados));
     }
 
 }
