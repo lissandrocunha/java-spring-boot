81c1ae981e86fd45b27c9fc1a8cd4bf6d9f71758
lissandro.cunha <lissandro.cunha@ifood.com.br>
2023-03-29 16:51:34 -0300

    - create get method for medico.

--- src/main/java/dev/lissandrocunha/api/controller/MedicoController.java
+++ src/main/java/dev/lissandrocunha/api/controller/MedicoController.java
@@ -1,15 +1,19 @@
 package dev.lissandrocunha.api.controller;
 
 import dev.lissandrocunha.api.medico.DadosCadastroMedico;
+import dev.lissandrocunha.api.medico.DadosListagemMedico;
 import dev.lissandrocunha.api.medico.Medico;
 import dev.lissandrocunha.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,8 +27,17 @@ public class MedicoController {
     @Transactional
     public void cadastrar(
             @RequestBody @Valid DadosCadastroMedico dados
-    ){
+    ) {
         medicoRepository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return medicoRepository
+                .findAll()
+                .stream()
+                .map(DadosListagemMedico::new)
+                .toList();
     }
 
 }
