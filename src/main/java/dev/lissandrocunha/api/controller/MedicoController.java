be2a5dd2686da60c489064860297b7d15be15174
lissandro.cunha <lissandro.cunha@ifood.com.br>
2023-03-30 13:59:30 -0300

    - create delete médico

--- src/main/java/dev/lissandrocunha/api/controller/MedicoController.java
+++ src/main/java/dev/lissandrocunha/api/controller/MedicoController.java
@@ -11,7 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -52,5 +54,14 @@ public class MedicoController {
     ){
         var medico = medicoRepository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(
+            @PathVariable Long id
+    ){
+        medicoRepository
+                .deleteById(id);
     }
 }
