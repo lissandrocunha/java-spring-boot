940e775644bf73ab7becfcc4dac21ddbac67e556
lissandro.cunha <lissandro.cunha@ifood.com.br>
2023-03-31 17:26:46 -0300

    - configure spring authentication user before spring authentication filter

---
+++ src/main/java/dev/lissandrocunha/api/infra/security/SecurityFilter.java
@@ -0,0 +1,56 @@
+package dev.lissandrocunha.api.infra.security;
+
+import dev.lissandrocunha.api.domain.usuario.Usuario;
+import dev.lissandrocunha.api.domain.usuario.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        var token = recuperarToken(request);
+
+        if (token != null){
+            var subject = tokenService.getSubject(token);
+            var usuario = usuarioRepository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null,usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request,response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.toLowerCase().startsWith("bearer "))
+        {
+            return authorizationHeader.substring(authorizationHeader.indexOf(' ' + 1));
+        }
+        return authorizationHeader;
+    }
+
+
+}
